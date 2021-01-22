274f0d05e432925b6c9799c8a4945e0116f94b70
Crain Lei <crain1.lei@gmail.com>
2021-01-23 07:52:35 +0800

    python: tts.py: one small python script for TTS
    Signed-off-by: Crain Lei <crain1.lei@gmail.com>

---
+++ tts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import tempfile
+from gtts import gTTS
+
+mt = input("")
+language = 'zh-CN'
+voice = gTTS(text=mt, lang=language, slow=False)
+new_file, path = tempfile.mkstemp()
+voice.save(path)
+os.system("mplayer " +  path + ">/dev/null 2>&1")
+
+os.remove(path)
