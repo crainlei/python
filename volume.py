3677b299785a876afda0cae772da3d2c7c7157e6
Crain Lei <crain1.lei@gmail.com>
2021-01-25 11:29:49 +0800

    python: volume.py: compute volume
    Signed-off-by: Crain Lei <crain1.lei@gmail.com>

---
+++ volume.py
@@ -0,0 +1,7 @@
+ 
+ 
+print("Input a length and a width and a height ")
+length = float(input("length:" ))
+width = float(input("width:" ))
+height = float(input("height:"))
+print("volume",(length*width*height))
